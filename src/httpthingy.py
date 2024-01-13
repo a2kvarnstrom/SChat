735940aeb209cd3e3bf094264bd0085262700b05
a2kvarnstrom <50154639+a2kvarnstrom@users.noreply.github.com>
2024-01-13 20:37:26 +0100

    made the server work yay
    port: 1199

--- src/httpthingy.py
+++ src/httpthingy.py
@@ -1,7 +1,7 @@
 import http.client
 
 def connect():
-    connection = http.client.HTTPConnection("192.168.1.213")
+    connection = http.client.HTTPConnection("192.168.1.213", 1199, timeout=10)
     connection.request("GET", "/")
     response = connection.getresponse()
     print("Status: {} \nReason: {}".format(response.status, response.reason))
