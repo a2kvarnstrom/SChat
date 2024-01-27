eb15e2fa4de49c81b045fb61cbe294042b9fedaf
a2kvarnstrom <50154639+a2kvarnstrom@users.noreply.github.com>
2024-01-28 00:23:56 +0100

    Change some things
    post requests now yay

--- src/httpthingy.py
+++ src/httpthingy.py
@@ -1,8 +1,8 @@
 import http.client
 
 def connect():
-    c =  http.client.HTTPConnection("192.168.1.213", 1199, timeout=10)
-    c.request("GET", "/")
+    c = http.client.HTTPConnection("192.168.0.123", 1199, timeout=10)
+    c.request("POST", "/")
     response = c.getresponse()
     print("Status: {} \nReason: {}".format(response.status, response.reason))
     connect.close = c.close()
--- src/main.py
+++ src/main.py
@@ -1,24 +1,31 @@
 import httpthingy as c
 import PySimpleGUI as sg 
+import hashlib
 
+def passcrypt():
+    print("hi")
+   
 c.connect()
 
-sg.theme("DarkAmber")
+sg.theme("Black")
 layout = [
     [sg.Text("Enter your Username"), sg.InputText()],
+    [sg.Text("Enter your Password"), sg.InputText()],
     [sg.Button("Enter"), sg.Button("Cancel")]
 ]
 curwin = sg.Window("Login", layout)
 event, values = curwin.read()
 while True:
     if event == "Enter":
-        print("this thing works")
+        print("this thing works\nUsername: " + values[0])
         break
     else:
         curwin.close()
+        c.connect.close
+        exit()
 curwin.close()
 layout = [
-    [sg.Text("Choose Recipient"), sg.ButtonMenu("Button1", ['Menu', ['&Pause Graph', 'Menu item::optional_key']])],
+    [sg.Text("Choose Recipient"), sg.Combo(values=['User1', 'User2'])],
     [sg.Button("button")]
 ]
 curwin = sg.Window("Chats", layout)
