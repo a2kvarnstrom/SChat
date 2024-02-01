58ea20ba7e2db080d4c0eecfe4d0cfbec849d8db
a2kvarnstrom <50154639+a2kvarnstrom@users.noreply.github.com>
2024-02-01 18:13:54 +0100

    removed comment

--- src/httpthingy.py
+++ src/httpthingy.py
@@ -44,18 +44,3 @@ def Register(u, p):
     d = u + ":" + c
     e = post("register", d)
     return e
-
-'''
-import aiohttp
-import asyncio
-
-async def post_request():
-    url = "http://uxhebxje.ddns.net:1199"
-    data = {"key": "value"}
-
-    async with aiohttp.ClientSession() as session:
-        response = await session.post(url, json=data)
-        print(await response.json())
-
-asyncio.run(post_request())
-''' 
