1ef2c20c86bbe1b992369259787a6b0d6003b9fb
a2kvarnstrom <50154639+a2kvarnstrom@users.noreply.github.com>
2024-03-07 22:46:57 +0100

    formatting less confusing

--- src/httpthingy.py
+++ src/httpthingy.py
@@ -13,7 +13,7 @@ def connect():
     print("Status: {} \nReason: {}".format(response.status, response.reason))
 
 def close():
-    connection.close
+    connection.close()
 
 def post(req, val, doPrint=True):
     a = '{"type": "%s", "value": %s}' % (req, val)
@@ -25,41 +25,41 @@ def post(req, val, doPrint=True):
     return response.text
 
 def passman(u, p):
-    salt = post("salt", str('"' + u + '"'))
+    salt = post("salt", '"%s"' % u)
     if salt == "No user found":
         return False
     ap = bytes(p + salt, 'utf-8')
-    send = hashlib.sha3_512(ap).hexdigest() + ":" + u
-    a = post("login", str('"' + send + '"'))
+    send = "%s:%s" % (hashlib.sha3_512(ap).hexdigest(), u)
+    a = post("login", '"%s"' % send)
     return a
 
 def Register(u, p):
     salt = hashlib.sha3_384(bytes(u + p, 'utf-8')).hexdigest()
     b = p + salt
     c = hashlib.sha3_512(bytes(b, 'utf-8')).hexdigest()
-    d = salt + ":" + c + ":" + u
-    e = post("register", str('"' + d + '"'))
+    d = "%s:%s:%s" % (salt, c, u)
+    e = post("register", '"%s"' % d)
     if e == "Username Already Taken":
         return "uname unav"
     return e
 
 def getUsers(u):
-    a = post("GetUsers", str('"' + u + '"'))
+    a = post("GetUsers", '"%s"' % u)
     return a
 
 def getMsgHistory(r, u):
     global msgHistory
-    msgHistory = post("GetMsgHistory", str('{"s":"' + u + '", "r":"' + r + '"}'), doPrint=False)
+    msgHistory = post("GetMsgHistory", '{"s":"%s", "r":"%s"}' % (u, r), doPrint=False)
     return msgHistory
 
 def send(u, s, r):
-    a = '{"msg":"' + s + '", "sender":"' + u + '", "recipient":"' + r + '"}'
+    a = '{"msg":"%s", "sender":"%s", "recipient":"%s"}' % (s, u, r)
     b = post("send", a)
     return b
 
 def messagePoll(u, r):
     try:
-        a = post("getNewMsgs", '"' + u + ':' + r + '"', doPrint=False)
+        a = post("getNewMsgs", '"%s:%s"' % (u, r), doPrint=False)
     except requests.exceptions.ConnectionError:
         return "Connection Error"
     return a
